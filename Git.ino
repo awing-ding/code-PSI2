6905ab05da7324ffc109e17dc7e2202f1af4aaef
ordi2118852 <ordi2118852@educ.normandie.fr>
2023-03-22 09:52:50 +0100

    22/03/2023

--- Git.ino
+++ Git.ino
@@ -7,47 +7,64 @@
 
 
 short int micVal ;
+void function_inutile();
 
-Deque <int> micValQ ;
+Deque <unsigned char> micValQ ;
 
-
+long unsigned time1;
 
 void setup() {
     Serial.begin(9600);
     pinMode(LED, OUTPUT);
     pinMode(MIC, INPUT);
+    for (int i = 0; i < 199; i++ ){
+    function_inutile();    
+    }
+    time1 = micros();
+    function_inutile2();
+    int time = micros()-time1;
+    Serial.println(time);
     
 }
 
-void loop() {
+void loop() {}
+
+void function_inutile2() {
     micVal = pow(analogRead(MIC), 2);
-    Serial.print("mic:");
-    Serial.println(micVal);
-    if (micVal >= REF or micVal <= REF2) {
-        micValQ.push_back(1);
-    }
-    else {
-        micValQ.push_back(0);
-    }
+    //Serial.print("mic:");
+    //Serial.println(micVal);
+    micValQ.push_back(micVal >= REF or micVal <= REF2);
     if (micValQ.count() > 100) {
         micValQ.pop_front();
     }
-    int sum = 0;
+    unsigned char sum = 0;
     for (int i = 0; i < micValQ.count(); i++) {
         sum += micValQ[i];
     }
-
-    if (sum > 14 and sum < 60) {
-        digitalWrite(LED, HIGH);
-    }
-    else {
-        digitalWrite(LED, LOW);
-    }
+    digitalWrite(LED, sum > 14 and sum < 60);
 
     //Serial.print("sum: ");
     //Serial.println(sum);
 
-    delay(1);
+    //delay(1);
+}
 
-    
+void function_inutile() {
+    micVal = pow(analogRead(MIC), 2);
+    //Serial.print("mic:");
+    //Serial.println(micVal);
+    micValQ.push_back(micVal >= REF or micVal <= REF2);
+    if (micValQ.count() > 100) {
+        micValQ.pop_front();
+    }
+    unsigned char sum = 0;
+    for (int i = 0; i < micValQ.count(); i++) {
+        sum += micValQ[i];
+    }
+    digitalWrite(LED, sum > 14 and sum < 60);
+
+    //Serial.print("sum: ");
+    //Serial.println(sum);
+
+    //delay(1);
 }
