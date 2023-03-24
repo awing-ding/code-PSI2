7624a79a9e93dcabede719440c7841da4a7efc49
awing_ding <awingding.win@gmail.com>
2023-03-24 08:57:38 +0100

    no more globals

---
+++ local.ino
@@ -0,0 +1,44 @@
+#include "lib/deque.h"
+
+#define LED 8
+#define MIC A0
+#define REF 735 //refrence
+#define REF2 725
+
+
+
+
+//A function that reads the value of the microphone and compares it to a reference value. If the value is greater than the reference value, it turns on the LED.
+Deque <unsigned char> function_inutile(Deque <unsigned char> micValQueue) {
+    short int micVal;
+    micVal = pow(analogRead(MIC), 2);
+    micValQueue.push_back(micVal >= REF or micVal <= REF2);
+    if (micValQueue.count() > 100) {
+        micValQueue.pop_front();
+    }
+    unsigned char sum = 0;
+    for (int i = 0; i < micValQueue.count(); i++) {
+        sum += micValQueue[i];
+    }
+    digitalWrite(LED, sum > 14 and sum < 60);
+    return micValQueue;
+}
+
+void setup() {
+    long unsigned time1;
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+    pinMode(MIC, INPUT);
+    Deque <unsigned char> micValQueue;
+    for (int i = 0; i < 199; i++ ){
+        time1 = micros();
+        micValQueue = function_inutile(micValQueue);    
+        int time = micros()-time1;
+        Serial.println(time);
+    }    
+}
+
+void loop() {}
+
+
+
