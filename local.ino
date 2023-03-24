8f47a1750ac34110d26a9287b9c8585ea09cc8c2
awing_ding <awingding.win@gmail.com>
2023-03-24 11:04:17 +0100

    deleting queue

--- local.ino
+++ local.ino
@@ -1,5 +1,3 @@
-#include "lib/deque.h"
-
 #define LED 8
 #define MIC A0
 #define REF 735 //reference -> less than 735 is the value of the microphone when there is no sound. It is used as a refe
@@ -7,19 +5,23 @@
 
 #define REF3 10 // the number of time the microphone value is greater than the reference value to activate the system
 #define REF4 60 // more than that should be a microphone error
-#define MAX_QUEUE_SIZE 75 //the size of the queue
+#define MAX_QUEUE_SIZE 100 //the size of the queue
 
 //A function that reads the value of the microphone and compares it to a reference value. If the value is greater than the reference value, it turns on the LED.
-Deque <unsigned char> function_inutile(Deque <unsigned char> micValQueue) {
+char function_inutile(char count) {
     short int micVal;
     micVal = pow(analogRead(MIC), 2);
-    micValQueue.push_back(micVal >= REF or micVal <= REF2);
-    unsigned char sum = 0;
-    for (int i = 0; i < micValQueue.count(); i++) {
-        sum += micValQueue[i];
+    if (micVal >= REF or micVal <= REF2){
+        count ++;
+        if (count > 100){
+            count  -= 2;
+        }
     }
-    digitalWrite(LED, sum > REF3 and sum < REF4);
-    return micValQueue;
+    else {
+        count --;
+    }
+    digitalWrite(LED, count > REF3 and count < REF4);
+    return count;
 }
 
 void setup() {
@@ -27,10 +29,10 @@ void setup() {
     Serial.begin(9600);
     pinMode(LED, OUTPUT);
     pinMode(MIC, INPUT);
-    Deque <unsigned char> micValQueue(MAX_QUEUE_SIZE);
+    char count = 0;
     for (int i = 0; i < 199; i++ ){ //some test don't worry
         time1 = micros();
-        micValQueue = function_inutile(micValQueue);    
+        count = function_inutile(count);    
         int time = micros()-time1;
         Serial.println(time);
     }    
