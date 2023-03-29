02f0a0fbeb3d06fe43f0b7519d45970747669ddc
awing_ding <awingding.win@gmail.com>
2023-03-29 08:52:37 +0200

    some changes

--- local.ino
+++ local.ino
@@ -2,7 +2,8 @@
 
 #define LED 8
 #define MIC A0
-#define REF 735 //reference -> less than 735 is the value of the microphone when there is no sound. It is used as a refe
+//should be change as sensibility has changed
+#define REF 735 //reference -> less than 735 is the value of the microphone when there is no sound. It is used as a refe 
 #define REF2 725 //reference -> more than 725 is the value of the microphone when there is no sound. It is used as a refe
 
 #define REF3 10 // the number of time the microphone value is greater than the reference value to activate the system
@@ -10,28 +11,27 @@
 #define MAX_QUEUE_SIZE 100 //the size of the queue
 
 //A function that reads the value of the microphone and compares it to a reference value. If the value is greater than the reference value, it turns on the LED.
-Deque <unsigned char> function_inutile(Deque <unsigned char> micValQueue) {
-    int micVal;
-    unsigned char count = micValQueue.pop_back();
-    micVal = pow(analogRead(MIC), 2); //trying to have more visible differences TODO: see if it's really useful
-    unsigned char trigger = micVal > REF or micVal < REF2;
-    micValQueue.push_back(trigger);
+Deque <unsigned char> function_inutile(Deque <unsigned char> micValueQueue) {
+    int micValue = analogRead(MIC);
+    unsigned char count = micValueQueue.pop_back();
+    unsigned char trigger = micValue > REF or micValue < REF2;
+    micValueQueue.push_back(trigger);
     count += trigger;
-    micValQueue.push_back(count);
+    micValueQueue.push_back(count);
     digitalWrite(LED, count > REF3 and count < REF4);
-    return micValQueue;
+    return micValueQueue;
 }
 
 void setup() {
     long unsigned time1;
     unsigned int time;
-    Serial.begin(4800); //for debug, we should try to reduce the baud rate
+    Serial.begin(9600); //for debug, we should try to reduce the baud rate
     pinMode(LED, OUTPUT);
     pinMode(MIC, INPUT);
-    Deque <unsigned char> micValQueue(MAX_QUEUE_SIZE);
+    Deque <unsigned char> micValueQueue(MAX_QUEUE_SIZE);
     for (int i = 0; i < 199; i++ ){ //some test don't worry
         time1 = micros();
-        micValQueue = function_inutile(micValQueue);    
+        micValueQueue = function_inutile(micValueQueue);    
         time = micros()-time1;
         Serial.println(time);
     }    
