a3296a8ff83d817224177b4913ab0e57e1f07731
awing_ding <awingding.win@gmail.com>
2023-03-29 09:24:01 +0200

    going into prod ?

--- local.ino
+++ local.ino
@@ -2,9 +2,9 @@
 
 #define LED 8
 #define MIC A0
-//sensibility has changed so ref should be changed too
-#define REF 735 //reference -> less than 735 is the value of the microphone when there is no sound. It is used as a refe
-#define REF2 725 //reference -> more than 725 is the value of the microphone when there is no sound. It is used as a refe
+
+#define REF 685 //reference -> less than it is the value of the microphone when there is no sound. It is used as a refe
+#define REF2 675 //reference -> more than it is the value of the microphone when there is no sound. It is used as a refe
 
 #define REF3 10 // the number of time the microphone value is greater than the reference value to activate the system
 #define REF4 60 // more than that should be a microphone error
@@ -22,9 +22,9 @@ void function_inutile(Deque <unsigned char> * const micValQueue) {
 }
 
 void setup() {
-    long unsigned time1;
-    unsigned int time;
-    Serial.begin(9600); //for debug, we should try to reduce the baud rate
+    //long unsigned time1;
+    //unsigned int time;
+    //Serial.begin(9600); //for debug, we should try to reduce the baud rate
     pinMode(LED, OUTPUT);
     pinMode(MIC, INPUT);
     Deque <unsigned char> micValQueue(MAX_QUEUE_SIZE);
@@ -43,8 +43,8 @@ void setup() {
 
 void pseudo_main(Deque <unsigned char> * const micValQueuePtr){
     while (1){
-        //function_inutile(micValQueuePtr);
-        Serial.println(analogRead(MIC));
+        function_inutile(micValQueuePtr);
+        //Serial.println(analogRead(MIC));
     }
 }
 void loop() {}
