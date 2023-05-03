a209f41b03b31da5d2f5f959e038c10f8bfce772
awing-ding <66324442+awing-ding@users.noreply.github.com>
2023-05-03 08:25:06 +0200

    Update local.h

--- local.h
+++ local.h
@@ -1,2 +1,3 @@
 void is_there_alarm(Deque <unsigned char> * const queue_of_time_it_was_triggered);
 void fill_queue(Deque <unsigned char> * const queue_of_time_it_was_triggered);
+void pseudo_main(Deque <unsigned char> * const pointer_of_triggers_queue);
