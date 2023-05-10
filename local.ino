355db2a9af0353868b8942562a2c1abadb44a55a
ShaneTiltran <123542466+ShaneTiltran@users.noreply.github.com>
2023-05-10 08:09:37 +0200

    Update local.ino

--- local.ino
+++ local.ino
@@ -1,71 +1,67 @@
-//lib with the deque class
-#include "lib/deque.h" 
+#include <arduinoFFT.h>
 
-//import function prototype
-#include "local.h"
+#define SAMPLES 512             // nombre d'échantillons
+#define SAMPLING_FREQUENCY 44100 // fréquence d'échantillonnage en Hz
 
-//the pins
-#define LED 8
-#define MIC A0 
+int ledPin = 13;              // broche de la LED
+int micPin = A0;              // broche du microphone
 
-//references to detect a trigger
-#define REF 39
-#define REF2 41
+arduinoFFT FFT = arduinoFFT();  // Créer un objet FFT
 
-//the number of time the microphone value is greater or inferior than the reference value to activate the system
-#define REF3 20 // the number of time the microphone value is greater than the reference value to activate the system
-#define REF4 60 // more than that should be a microphone error or dumb ppl that hit the mic
-
-//the size of the queue
-#define MAX_QUEUE_SIZE 100 
-
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+double freqValues[100] = {0};
+int freqIndex = 0;
+int freqCount = 0;
 
 void setup() {
-    //sets the pins
-    pinMode(LED, OUTPUT);
-    pinMode(MIC, INPUT);
-
-    //creates the queue, fill it and get a pointer to it
-    Deque <unsigned char> queue_of_time_it_was_triggered(MAX_QUEUE_SIZE);
-    fill_queue(&queue_of_time_it_was_triggered);
-    Deque <unsigned char> * pointer_of_triggers_queue = &queue_of_time_it_was_triggered;
-
-    //starts the main function, not the arduino loop but a home-made one
-    pseudo_main(pointer_of_triggers_queue);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  ADCSRA |=  bit (ADPS2) | bit (ADPS1); // définir la fréquence d'échantillonnage à 44,1 kHz
 }
 
-//the main function
-void pseudo_main(Deque <unsigned char> * const pointer_of_triggers_queue){
-    //a forever loop that checks if there is an alarm
-    while (1){
-        is_there_alarm(pointer_of_triggers_queue);
+void loop() {
+  // Lecture des échantillons analogiques
+  for (int i = 0; i < SAMPLES; i++) {
+    vReal[i] = analogRead(micPin);  // Lecture de l'entrée analogique du microphone
+    vImag[i] = 0;
+  }
+
+  // Appliquer une fenêtre de Hamming aux données
+  for (int i = 0; i < SAMPLES; i++) {
+    vReal[i] = vReal[i] * (0.54 - 0.46 * cos(2 * PI * i / (SAMPLES - 1)));
+  }
+  
+  // Exécution de la transformée de Fourier
+  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+  
+  // Ajouter la fréquence à la liste des fréquences précédentes
+  freqValues[freqIndex] = peak;
+  freqIndex = (freqIndex + 1) % 100;
+  
+  // Calculer le nombre de fréquences comprises entre 8000 et 9000 Hz dans les 100 dernières valeurs
+  freqCount = 0;
+  for (int i = 0; i < 50; i++) {
+    if (freqValues[i] >= 8000 && freqValues[i] <= 11000) {
+      freqCount++;
     }
+  }
+
+  // Vérification si le nombre de fréquences entre est suffisant pour étre une alarme incendie
+  if (freqCount >= 50/5) {
+    // Allumer la LED si la fréquence est détectée
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  // Affichage de la fréquence
+  Serial.print("Frequency: ");
+  Serial.print(peak);
+  Serial.print(" Hz, Frequencies between 8000 and 11000 Hz: ");
+  Serial.print(freqCount);
+  Serial.println();
+
+  delay(100);
 }
-
-//A function that reads the value of the microphone and compares it to a reference value. If the value is greater than the reference value, it turns on the LED.
-void is_there_alarm(Deque <unsigned char> * const queue_of_time_it_was_triggered) {
-    unsigned char number_of_time_triggered = queue_of_time_it_was_triggered->pop_back();
-    number_of_time_triggered -= queue_of_time_it_was_triggered->pop_front();
-
-    //reads the value of the microphone and compares it to a reference value
-    int value_of_mic = analogRead(MIC);
-    unsigned char trigger = (value_of_mic > REF or value_of_mic < REF2); //1 if there is a trigger, 0 if not
-    queue_of_time_it_was_triggered->push_back(trigger);
-    //use a counter value to be faster
-    number_of_time_triggered += trigger;
-    queue_of_time_it_was_triggered->push_back(number_of_time_triggered);
-
-    //if the number of time the microphone value is greater than the reference value is greater than REF3, it turns on the LED
-    //as more than REF4 is considered as a microphone error, it turns off the LED
-    digitalWrite(LED, number_of_time_triggered > REF3 and number_of_time_triggered < REF4);
-}
-
-void fill_queue(Deque <unsigned char> * const queue_of_time_it_was_triggered){
-    for (int i = 0; i < MAX_QUEUE_SIZE; i++){
-        queue_of_time_it_was_triggered->push_back(0);
-    }
-}
-
-//as we didn't really know how it works, we made our own loop
-//but as it's mandatory to have it, we made it empty
-void loop() {}
